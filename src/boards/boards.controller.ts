708389638320eb42752809e61f756eecb924941a
HC-kang <weston0713@gmail.com>
2023-01-22 23:28:49 +0900

    board 기본 완성

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -12,6 +12,7 @@ import {
 import { CreateBoardDto } from './\bdto/create-board.dto';
 import { Board, BoardStatus } from './board.model';
 import { BoardsService } from './boards.service';
+import { BoardStatusValidationPipe } from './pipes/board-status-validation.pipe';
 
 @Controller('boards')
 export class BoardsController {
@@ -41,7 +42,7 @@ export class BoardsController {
   @Patch('/:id/status')
   updateBoardStatus(
     @Param('id') id: string,
-    @Body('status') status: BoardStatus,
+    @Body('status', BoardStatusValidationPipe) status: BoardStatus,
   ): Board {
     return this.boardsService.updateBoardStatus(id, status);
   }

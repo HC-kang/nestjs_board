c6411f348a60adf4def41cd2c6b617ec143265af
HC-kang <weston0713@gmail.com>
2023-01-23 19:05:45 +0900

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardsModule } from './boards/boards.module';
 import { typeOrmConfig } from './configs/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+// import { validate } from './util/validate/env.validation';
 
 @Module({
-  imports: [BoardsModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.${process.env.NODE_ENV}.env`],
+      // validate,
+    }),
+    BoardsModule,
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+  ],
 })
 export class AppModule {}
--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -1,13 +1,21 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Board } from 'src/boards/board.entity';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'heechan',
-  password: 'root',
-  database: 'board_app',
-  entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
-  synchronize: true,
-};
+export const typeOrmConfig: TypeOrmModuleOptions =
+  process.env.NODE_ENV === 'test'
+    ? {
+        type: 'sqlite',
+        database: 'test.sqlite',
+        entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
+        synchronize: true,
+      }
+    : {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'heechan',
+        password: 'root',
+        database: 'board_app',
+        entities: [__dirname + '/../**/*.entity.{js,ts}', Board],
+        synchronize: true,
+      };

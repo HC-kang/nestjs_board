9eae9e8a665f8373cca694e03b6b47682246dddd
HC-kang <weston0713@gmail.com>
2023-01-23 15:23:11 +0900

    Feat. Passport, AuthGuard 추가

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,23 @@ import { TypeOrmExModule } from 'src/database/typeorm-ex.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([UserRepository])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'topSecret1234',
+      signOptions: {
+        expiresIn: 60 * 60,
+      },
+    }),
+    TypeOrmExModule.forCustomRepository([UserRepository]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,24 +4,34 @@ import { AuthCredentialDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialDto: AuthCredentialDto): Promise<User> {
     return this.userRepository.createUser(authCredentialDto);
   }
 
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // generate jwt token ( Secret, Payload )
+      const payload = { username };
+      const accessToken = await this.jwtService.sign(payload);
+
+      return {
+        accessToken,
+      };
     } else {
       throw new UnauthorizedException('Invalid credentials');
     }
